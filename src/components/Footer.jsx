40a89808c778d04a462d7d08029c60bce3e35a7d
suraj savle <surajsavle143@gmail.com>
2026-03-17 13:30:15 +0530

    added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='bg-black flex items-center justify-center text-white p-5'>"I am Groot… 🌍 improve layout and card ,  </div>
+  )
+}
+
+export default Footer
+
--- src/components/HabitsCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function HabitsCard() {
-  return (
-    <div>HabitsCard</div>
-  )
-}
-
-export default HabitsCard;
