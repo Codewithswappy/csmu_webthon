8f4d5adeecc81846dfc1c26fc3fdb31d1e7a85f8
suraj savle <surajsavle143@gmail.com>
2026-03-17 11:22:03 +0530

    reamoved app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './App.css'
 import { Hero } from './pages/hero'
 
 function App() {
--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -1,8 +1,7 @@
-
+import React from 'react'
 export const Hero = () => {
   return (
     <div className='h-screen w-full bg-red-800'>    
-    
     </div>
   )
 }
