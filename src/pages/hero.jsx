910c00116188d7011c1240ec2c931318a0be4410
Codewithswappy <skalambe520@gmail.com>
2026-03-17 12:00:17 +0530

    change hero img

--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 export const Hero = () => {
   return (
-    <div className='bg-[url("/hero2.jpg")] bg-cover  bg-top h-screen w-full'>    
-    
+    <div className='bg-[url("/groot.jpg")]  bg-cover  bg-top h-screen w-full'>    
+    <div className='max-w-5xl mx-auto text-center text-neutral-800 h-screen flex flex-col justify-center items-center gap-4 pb-40'>
+        <div>
+            <h1 className='text-5xl font-bold text-[#5FDB15]'>Save the Earth.</h1>
+        <p>Even the smallest seed can grow into a mighty tree.</p>
+        </div>
+
+        <button className='p-4 border'>
+            Plant a Tree Today
+        </button>
+    </div>
     </div>
   )
 }
